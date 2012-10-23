ff0fd1c185a7847d352c8df0f407dc5ab081f1f8
Jakub Konka <kubkon@gmail.com>
2012-10-23 13:54:50 +0100

    Add option for transient/steady-state analysis.

--- analyze.py
+++ analyze.py
@@ -8,30 +8,47 @@ Copyright (c) 2012 University of Strathclyde. All rights reserved.
 """
 import argparse
 import csv
-from itertools import cycle
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import os.path
 import scipy.stats as stats
+import sys
 
 
 ### Parse command line arguments
-parser = argparse.ArgumentParser(description="DM simulation -- Analysis script")
+parser = argparse.ArgumentParser(description="DM simulation -- Statistical analysis script")
 parser.add_argument('input_dir', help='directory with simulation results')
+parser.add_argument('mode', help='transient or steady-state')
 parser.add_argument('--confidence', dest='confidence', default=0.99,
                     type=float, help='confidence value (default: 0.99)')
 args = parser.parse_args()
 input_dir = args.input_dir
+mode = args.mode
 confidence = args.confidence
 
-### Merge results from files
-# Get files (as strings)
+### Common params
+# Output dir names
+transient_dir = 'transient'
+ss_dir = 'steady-state'
+# File names and paths
 extension = ".out"
-file_names = set([f[:f.find(extension)] for _, _, files in os.walk(input_dir) for f in files if f.endswith(extension)])
-file_paths = [os.path.join(root, f) for root, _, files in os.walk(input_dir) for f in files if f.endswith(extension)]
-# Initial processing of data (includes warm-up period)
+file_names = set([f[:f.find(extension)] for root, _, files in os.walk(input_dir) for f in files \
+                  if f.endswith(extension) and transient_dir not in root and ss_dir not in root])
+file_paths = [os.path.join(root, f) for root, _, files in os.walk(input_dir) for f in files \
+              if f.endswith(extension) and transient_dir not in root and ss_dir not in root]
+# Reference column
 ref_column = 'sr_number'
+# Ask for warm-up period index (if mode is steady-state)
+if mode.lower() == 'steady-state':
+  save_dir = input_dir + '/' + ss_dir
+  warmup = int(input('Warm-up period index: '))
+elif mode.lower() == 'transient':
+  save_dir = input_dir + '/' + transient_dir
+  warmup = 0
+else:
+  sys.exit('Unknown mode specified.')
+
+### Merge results from files
 for name in file_names:
   # Read data from files
   data_in = []
@@ -41,13 +58,15 @@ for name in file_names:
         reader = csv.DictReader(f)
         dct = {}
         for row in reader:
-          for key in row:
-            val = float(row[key]) if key != ref_column else int(row[key])
-            dct.setdefault(key, []).append(val)
+          # Exclude data with index lower than specified warm-up period
+          if int(row[ref_column]) > warmup:
+            for key in row:
+              val = float(row[key]) if key != ref_column else int(row[key])
+              dct.setdefault(key, []).append(val)
         data_in.append(dct)
-  # Reduce...
   # Get number of replications (equal to number of dictionaries in data_in)
   repetitions = len(data_in)
+  # Map and reduce...
   # Compute mean
   zipped = zip(*[dct[key] for dct in data_in for key in dct.keys() if key != ref_column])
   means = list(map(lambda x: sum(x)/repetitions, zipped))
@@ -59,8 +78,7 @@ for name in file_names:
   # Compute confidence intervals for the mean
   cis = list(map(lambda x: x * stats.t.ppf(0.5 + confidence/2, repetitions-1), ses))
   # Save to a file
-  # Create save dir if doesn't exist
-  save_dir = input_dir + '/transient'
+  # Create save dir if doesn't exist already
   if not os.path.exists(save_dir):
     os.makedirs(save_dir)
   with open(save_dir + '/' + name + extension, 'w', newline='', encoding='utf-8') as f:
