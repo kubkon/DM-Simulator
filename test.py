9c2c6cad8f4b94d4d89bfab01dc5d67353d73852
Jakub Konka <kubkon@gmail.com>
2013-01-07 13:43:12 +0100

    Add master test script.
    Master test script allows running all tests from a single location.

---
+++ test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+test.py
+
+Created by Jakub Konka on 2013-01-07.
+Copyright (c) 2012 University of Strathclyde. All rights reserved.
+"""
+import unittest
+import simulator.tests.dm as dm
+import simulator.tests.sim as sim
+
+# Run tests
+# 1. Bidder class
+unittest.TextTestRunner(verbosity=2).run( \
+    unittest.TestLoader().loadTestsFromTestCase(dm.BidderTests))
+# 2. DMEventHandler class
+unittest.TextTestRunner(verbosity=2).run( \
+    unittest.TestLoader().loadTestsFromTestCase(dm.DMEventHandlerTests))
+# 3. SimulatorEngine class
+unittest.TextTestRunner(verbosity=2).run( \
+    unittest.TestLoader().loadTestsFromTestCase(sim.SimulationEngineTests))
+# 4. Event class
+unittest.TextTestRunner(verbosity=2).run( \
+    unittest.TestLoader().loadTestsFromTestCase(sim.EventTests))
+
